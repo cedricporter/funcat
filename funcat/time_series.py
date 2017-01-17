aa8525bf8477ff44c9cc20c62187e96bdc36d67f
Cedric Porter <cedricporter@gmail.com>
2017-01-17 18:53:30 +0800

    add base class from TimeSeries

--- funcat/time_series.py
+++ funcat/time_series.py
@@ -55,7 +55,7 @@ def get_series(val):
         return DuplicateNumericSeries(val).series
 
 
-class TimeSeries:
+class TimeSeries(object):
     '''
     https://docs.python.org/3/library/operator.html
     '''
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from pip.req import parse_requirements
 
 setup(
     name='funcat',
-    version='0.0.5',
+    version='0.0.6',
     description='funcat',
     packages=find_packages(exclude=[]),
     author='Hua Liang',
